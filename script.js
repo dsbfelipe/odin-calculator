2420ac7120244eb990b2767cb3e2cc5f4082b4f6
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2024-09-17 16:27:25 -0300

    Create divide function

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function subtract(a, b) {
 function multiply(a, b) {
   return a * b;
 }
+
+function divide(a, b) {
+  if (b === 0) {
+    alert("Cannot divide by 0");
+    return;
+  }
+  return a / b;
+}
